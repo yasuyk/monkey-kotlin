88ded71cbe5182e1cce3d7563cdd4d3fb0725637
Yasuyuki Oka <yasuyk@gmail.com>
2018-01-20 20:21:10 +0900

    Make reading source files

--- src/main/kotlin/monkey/main.kt
+++ src/main/kotlin/monkey/main.kt
@@ -1,10 +1,41 @@
 package monkey
 
+import monkey.`object`.Environment
+import monkey.`object`.isError
+import monkey.evaluator.eval
+import monkey.lexer.Lexer
+import monkey.parser.Parser
 import monkey.repl.Repl
+import java.io.File
 
 fun main(args: Array<String>) {
+    if (args.isEmpty()) {
+        repl()
+    } else {
+        interpret(args)
+    }
+}
+
+
+private fun repl() {
     val user = System.getProperty("user.name")
     println("Hello $user! This is the Monkey programming language!\n")
     println("Feel free to type in commands\n")
     Repl().start(System.`in`, System.out)
 }
+
+fun interpret(args: Array<String>) {
+
+    val input = StringBuffer().apply {
+        args.map { f -> append(File(f).bufferedReader().readText()) }
+    }.toString()
+    val p = Parser.newInstance(Lexer.newInstance(input)).parseProgram()
+    val env = Environment()
+    val ret = eval(p, env)
+    if (ret.isError()) {
+        ret?.let {
+            System.err.println(it.inspect())
+        }
+    }
+}
+
